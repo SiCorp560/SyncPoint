fca9a3e5debeae6fd935ebb89d14b330aa0f45b6
Simon Corpuz <scorpuz@andrew.cmu.edu>
2023-10-24 18:52:47 -0400

    Created application and added Firebase packages.

---
+++ SyncPoint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SyncPoint
+//
+//  Created by Simon Corpuz on 10/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SyncPoint/SyncPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncPointApp.swift
+//  SyncPoint
+//
+//  Created by Simon Corpuz on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SyncPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
