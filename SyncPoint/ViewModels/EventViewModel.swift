af8c1d6f3331a7220d097becd198bdd194d4a9ff
Ahmad Alhallaq <aalhalla@andrew.cmu.edu>
2023-11-07 10:47:21 -0500

    fixed array issue in create event

--- SyncPoint/ViewModels/EventViewModel.swift
+++ SyncPoint/ViewModels/EventViewModel.swift
@@ -16,51 +16,54 @@ class EventViewModel: ObservableObject {
   
   func add(_ event: Event, _ participants: [String?]) {
     // add the event to the event respository
-    print("----")
-    print("events before")
-    print(eventRepository.events)
     eventRepository.create(event)
-    //print(newEvent)
+    eventRepository.events.append(event)
+    
+    
     // add the event to the tbd events of each participant(including host)
-    //print(participants)
-    var newParticipants = participants.compactMap { $0 }
-    //print(newParticipants)
-    let users = userResposirty.users
-    let events = eventRepository.events
-    print("----")
-    print("events after")
-    print(events)
-//    for e in events {
-//      print(e)
-//      print("----")
-//    }
-    for participant in newParticipants {
-      //print("yesssss")
-      var user = users.filter { $0.id == participant }.first
-      //print(user?.first_name)
-      //print(user)
-      //user?.tbd_events.append("yesss")
-      //print(event.name)
-      var thisEvent = events.filter { $0.name == event.name }.first
-      user?.tbd_events.append(thisEvent?.name ?? "-1")
-      userResposirty.update(user!)
-    }
+    addToTBD(event, participants)
+
+
     
     // add availability instances
-    addAvailabilityRecords(event, participants)
+    //addAvailabilityRecords(event, participants)
   }
   
-  func addAvailabilityRecords(_ event: Event, _ participants: [String?]) {
-    let users = userResposirty.users
-    for participant in participants {
-      var thisUser = users.filter { $0.id == participant }.first
-      if let unwrappedUser = thisUser {
-        var userID = unwrappedUser.id ?? "-1"
-        var availability = Availability(user: userID, event:event.id ?? "-1", times:[], indicated:false)
-        availabilityRepository.create(availability)
-      }
+  func addToTBD(_ event: Event, _ participants: [String?]) {
+    eventRepository.get()
+    var newEvents = eventRepository.events
+    print(newEvents.count)
+    for e in newEvents {
+      print(e)
+      print("----")
     }
+    
+    // let newEventID =  need to save it to be able to use it below
+
+    
+//    var newParticipants = participants.compactMap { $0 }
+//    let users = userResposirty.users
+//    
+//    for participant in newParticipants {
+//      var user = users.filter { $0.id == participant }.first
+//      user?.tbd_events.append(newEventID ?? "-1")
+//      userResposirty.update(user!)
+//    }
   }
+  
+  
+  
+//  func addAvailabilityRecords(_ event: Event, _ participants: [String?]) {
+//    let users = userResposirty.users
+//    for participant in participants {
+//      var thisUser = users.filter { $0.id == participant }.first
+//      if let unwrappedUser = thisUser {
+//        var userID = unwrappedUser.id ?? "-1"
+//        var availability = Availability(user: userID, event:event.id ?? "-1", times:[], indicated:false)
+//        availabilityRepository.create(availability)
+//      }
+//    }
+//  }
   
 }
 
