c31124b81290e6c7271abff6d65b87f77a86e04e
Simon Corpuz <scorpuz@andrew.cmu.edu>
2023-12-12 12:36:21 -0500

    Started notifications work, currently attempting to find device token.

--- SyncPoint/SyncPointApp.swift
+++ SyncPoint/SyncPointApp.swift
@@ -5,13 +5,22 @@
 //  Created by Simon Corpuz on 10/24/23.
 //
 
+import UIKit
+import UserNotifications
 import SwiftUI
 import FirebaseCore
 import FirebaseAuth
 import GoogleSignIn
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Adding in a request for notifications
+        UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .sound, .alert]) { granted, _ in
+            guard granted else { return }
+            DispatchQueue.main.async {
+                application.registerForRemoteNotifications()
+            }
+        }
         FirebaseApp.configure()
         return true
     }
@@ -22,12 +31,31 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     ) -> Bool {
         return GIDSignIn.sharedInstance.handle(url)
     }
+    
+//    // MARK: Remote Notifications
+//    // This method simply allows us to see the device token so we can send PNs with our test server
+//    func application(_ application: UIApplication,
+//                   didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//        let token = deviceToken.reduce("") { $0 + String(format: "%02x", $1) }
+//        print("Token is \(token)")
+//    }
+    @MainActor
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        print("DEVICE TOKEN: \(deviceToken.reduce("") { $0 + String(format: "%02x", $1)})")
+    }
+    
 }
 
 @main
 struct SyncPointApp: App {
+    
+    
     // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     
     @StateObject var authViewModel = AuthenticationViewModel()
     
